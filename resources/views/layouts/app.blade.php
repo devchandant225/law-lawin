480daaa1ee76219164e32404902422919968a4a2
devchandan225 <chandant@taknify.com>
2025-08-27 21:14:06 +0545

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,24 +17,24 @@
     <link rel="preconnect" href="https://fonts.googleapis.com/">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 
-
-    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/vendors/bootstrap-select/bootstrap-select.min.css" />
-    <link rel="stylesheet" href="assets/vendors/animate/animate.min.css" />
-    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css" />
-    <link rel="stylesheet" href="assets/vendors/jquery-ui/jquery-ui.css" />
-    <link rel="stylesheet" href="assets/vendors/jarallax/jarallax.css" />
-    <link rel="stylesheet" href="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css" />
-    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.min.css" />
-    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.pips.css" />
-    <link rel="stylesheet" href="assets/vendors/tiny-slider/tiny-slider.css" />
-    <link rel="stylesheet" href="assets/vendors/procounsel-icons/style.css" />
-    <link rel="stylesheet" href="assets/vendors/slick/slick.css">
-    <link rel="stylesheet" href="assets/vendors/owl-carousel/css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="assets/vendors/owl-carousel/css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-select/bootstrap-select.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/fontawesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-ui/jquery-ui.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jarallax/jarallax.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.pips.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/tiny-slider/tiny-slider.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/procounsel-icons/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/css/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/css/owl.theme.default.min.css') }}" />
 
     <!-- template styles -->
-    <link rel="stylesheet" href="assets/css/procounsel.css" />
+    <link rel="stylesheet" href="{{ asset('assets/css/procounsel.css') }}" />
+
     @hasSection('head')
         @yield('head')
     @else
@@ -66,7 +66,7 @@
 
             <div class="logo-box">
                 <a href="{{ route('home') }}" aria-label="logo image">
-                    @if($globalProfile && $globalProfile->logo_url)
+                    @if ($globalProfile && $globalProfile->logo_url)
                         <img src="{{ $globalProfile->logo_url }}" width="155" alt="{{ config('app.name') }}" />
                     @else
                         <img src="assets/images/logo-light.png" width="155" alt="{{ config('app.name') }}" />
@@ -78,43 +78,43 @@
             <!-- /.mobile-nav__container -->
 
             <ul class="mobile-nav__contact list-unstyled">
-                @if($globalProfile && $globalProfile->email)
-                <li>
-                    <i class="fa fa-envelope"></i>
-                    <a href="mailto:{{ $globalProfile->email }}">{{ $globalProfile->email }}</a>
-                </li>
+                @if ($globalProfile && $globalProfile->email)
+                    <li>
+                        <i class="fa fa-envelope"></i>
+                        <a href="mailto:{{ $globalProfile->email }}">{{ $globalProfile->email }}</a>
+                    </li>
                 @endif
-                @if($globalProfile && $globalProfile->phone1)
-                <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="tel:{{ $globalProfile->phone1 }}">{{ $globalProfile->phone1 }}</a>
-                </li>
+                @if ($globalProfile && $globalProfile->phone1)
+                    <li>
+                        <i class="fa fa-phone-alt"></i>
+                        <a href="tel:{{ $globalProfile->phone1 }}">{{ $globalProfile->phone1 }}</a>
+                    </li>
                 @endif
             </ul><!-- /.mobile-nav__contact -->
             <div class="mobile-nav__social">
-                @if($globalProfile && $globalProfile->facebook_link)
-                <a href="{{ $globalProfile->facebook_link }}">
-                    <i class="icon-facebook"></i>
-                    <span class="sr-only">Facebook</span>
-                </a>
+                @if ($globalProfile && $globalProfile->facebook_link)
+                    <a href="{{ $globalProfile->facebook_link }}">
+                        <i class="icon-facebook"></i>
+                        <span class="sr-only">Facebook</span>
+                    </a>
                 @endif
-                @if($globalProfile && $globalProfile->linkedin_link)
-                <a href="{{ $globalProfile->linkedin_link }}">
-                    <i class="fab fa-linkedin"></i>
-                    <span class="sr-only">LinkedIn</span>
-                </a>
+                @if ($globalProfile && $globalProfile->linkedin_link)
+                    <a href="{{ $globalProfile->linkedin_link }}">
+                        <i class="fab fa-linkedin"></i>
+                        <span class="sr-only">LinkedIn</span>
+                    </a>
                 @endif
-                @if($globalProfile && $globalProfile->twitter_link)
-                <a href="{{ $globalProfile->twitter_link }}">
-                    <i class="icon-twitter"></i>
-                    <span class="sr-only">Twitter</span>
-                </a>
+                @if ($globalProfile && $globalProfile->twitter_link)
+                    <a href="{{ $globalProfile->twitter_link }}">
+                        <i class="icon-twitter"></i>
+                        <span class="sr-only">Twitter</span>
+                    </a>
                 @endif
-                @if($globalProfile && $globalProfile->youtube_link)
-                <a href="{{ $globalProfile->youtube_link }}">
-                    <i class="icon-youtube"></i>
-                    <span class="sr-only">Youtube</span>
-                </a>
+                @if ($globalProfile && $globalProfile->youtube_link)
+                    <a href="{{ $globalProfile->youtube_link }}">
+                        <i class="icon-youtube"></i>
+                        <span class="sr-only">Youtube</span>
+                    </a>
                 @endif
             </div><!-- /.mobile-nav__social -->
         </div>
@@ -140,33 +140,38 @@
         <span class="scroll-to-top__text">back top</span>
         <span class="scroll-to-top__wrapper"><span class="scroll-to-top__inner"></span></span>
     </a>
-    <script src="assets/vendors/jquery/jquery-3.7.1.min.js"></script>
-    <script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
-    <script src="assets/vendors/jarallax/jarallax.min.js"></script>
-    <script src="assets/vendors/jquery-ui/jquery-ui.js"></script>
-    <script src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
-    <script src="assets/vendors/jquery-appear/jquery.appear.min.js"></script>
-    <script src="assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
-    <script src="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
-    <script src="assets/vendors/jquery-validate/jquery.validate.min.js"></script>
-    <script src="assets/vendors/nouislider/nouislider.min.js"></script>
-    <script src="assets/vendors/tiny-slider/tiny-slider.js"></script>
-    <script src="assets/vendors/wnumb/wNumb.min.js"></script>
-    <script src="assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
-    <script src="assets/vendors/wow/wow.js"></script>
-    <script src="assets/vendors/imagesloaded/imagesloaded.min.js"></script>
-    <script src="assets/vendors/isotope/isotope.js"></script>
-    <script src="assets/vendors/slick/slick.min.js"></script>
-    <script src="assets/vendors/tilt/tilt.jquery.js"></script>
-    <script src="assets/vendors/countdown/countdown.min.js"></script>
-    <script src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
-    <script src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
+    <script src="{{ asset('assets/vendors/jquery/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/bootstrap-select/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jarallax/jarallax.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-ui/jquery-ui.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-appear/jquery.appear.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-validate/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/nouislider/nouislider.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/tiny-slider/tiny-slider.js') }}"></script>
+    <script src="{{ asset('assets/vendors/wnumb/wNumb.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/owl-carousel/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/wow/wow.js') }}"></script>
+    <script src="{{ asset('assets/vendors/imagesloaded/imagesloaded.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/isotope/isotope.js') }}"></script>
+    <script src="{{ asset('assets/vendors/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/tilt/tilt.jquery.js') }}"></script>
+    <script src="{{ asset('assets/vendors/countdown/countdown.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-circleType/jquery.circleType.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-lettering/jquery.lettering.min.js') }}"></script>
+
     <!-- gsap js -->
-    <script src="assets/vendors/gsap/gsap.js"></script>
-    <script src="assets/vendors/gsap/scrolltrigger.min.js"></script>
-    <script src="assets/vendors/gsap/splittext.min.js"></script>
-    <script src="assets/vendors/gsap/procounsel-split.js"></script>
+    <script src="{{ asset('assets/vendors/gsap/gsap.js') }}"></script>
+    <script src="{{ asset('assets/vendors/gsap/scrolltrigger.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/gsap/splittext.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/gsap/procounsel-split.js') }}"></script>
+
+    <!-- template js -->
+    <script src="{{ asset('assets/js/procounsel.js') }}"></script>
+
     <!-- template js -->
     <script src="assets/js/procounsel.js"></script>
     @stack('scripts')

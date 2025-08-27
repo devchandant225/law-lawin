4a96ff363e7271f99bcf8bc46f89439f6e836901
devchandan225 <chandant@taknify.com>
2025-08-28 01:12:33 +0545

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,10 +22,10 @@
         sectionDescription="Explore our comprehensive collection of legal practice, research papers, and expert insights covering various areas of law." />
 
     {{-- Portfolio Section --}}
-    <x-portfolio-section :portfolios="$portfolios" :showViewAll="true" :limit="10"
+    {{-- <x-portfolio-section :portfolios="$portfolios" :showViewAll="true" :limit="10"
         sectionTitle="<span class=''>Our Portfolio</span>"
         sectionSubtitle="Our Latest Work"
-        sectionDescription="Explore our diverse portfolio of successful projects and see how we bring ideas to life with creativity and expertise." />
+        sectionDescription="Explore our diverse portfolio of successful projects and see how we bring ideas to life with creativity and expertise." /> --}}
 
     {{-- Testimonial Section - Clients We Served --}}
     <x-testimonial-section :portfolios="$portfolios" :limit="8"

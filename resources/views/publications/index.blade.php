3b069397cf0c6d359dee6d54da787e664c89996a
devchandan225 <chandant@taknify.com>
2025-09-02 17:19:20 +0545

    new

--- resources/views/publications/index.blade.php
+++ resources/views/publications/index.blade.php
@@ -11,29 +11,10 @@
         :breadcrumbs="[['label' => 'Home', 'url' => url('/')], ['label' => 'Publications']]" />
 
     <section class="award-one">
-        <div class="container">
-            @if ($publications->isNotEmpty())
-                <div class="row gutter-y-30">
-                    @foreach ($publications as $index => $publication)
-                        <div class="col-lg-6 wow fadeInUp" data-wow-delay="{{ ($index % 2) * 100 }}ms">
-                            <div class="award-one__item">
-                                <div class="award-one__item__left">
-                                    <h2 class="award-one__item__title">
-                                        <a
-                                            href="{{ route('publication.show', $publication->slug) }}">{{ $publication->title }}</a>
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                <div class="text-center py-5">
-                    <h4>No publications available at the moment.</h4>
-                    <p>Please check back later for our latest publications and resources.</p>
-                </div>
-            @endif
-        </div>
-    </section>
+        {{-- Publications Section --}}
+        <x-publication-section :showViewAll="true" :limit="8" :showSearch="true"
+            sectionTitle="<span class=''>Publications</span>" sectionSubtitle="Legal Knowledge & Resources"
+            sectionDescription="Explore our comprehensive collection of legal publications, research papers, and expert insights covering various areas of law." />
 
+    </section>
 @endsection

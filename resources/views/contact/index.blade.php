2adf1383acd582bfc813d25efdf43885f8746c5a
devchandan225 <chandant@taknify.com>
2025-09-02 13:09:42 +0545

    new

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -26,8 +26,6 @@
         sectionDescription="We're here to help with any questions you might have. Feel free to reach out to us and we'll get back to you as soon as possible."
         :showSocialLinks="true" />
     <div class="google-map google-map__contact">
-        <iframe title="template google map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
-            class="map__contact" allowfullscreen></iframe>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.7498965995605!2d85.32228617453497!3d27.687025526389178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb191b975e3b81%3A0x60674a6cd53cbf4a!2sLawin%20and%20Partners!5e1!3m2!1sen!2snp!4v1756797619262!5m2!1sen!2snp" width="100%" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 @endsection

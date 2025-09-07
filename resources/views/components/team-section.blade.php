4e5e06aadd3fe79bce7b8c048594bf7717eee688
devchandan225 <chandant@taknify.com>
2025-09-07 17:45:04 +0545

    new

--- resources/views/components/team-section.blade.php
+++ resources/views/components/team-section.blade.php
@@ -4,7 +4,7 @@
 
         @if ($teams->isNotEmpty())
             <!-- Team Grid -->
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 mb-12">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-16 gap-y-4 mb-12">
                 @foreach ($teams as $index => $member)
                     <div class="team-card-wrapper w-[14rem]" data-aos="fade-up" data-aos-duration="800"
                         data-aos-delay="{{ $index * 100 }}">

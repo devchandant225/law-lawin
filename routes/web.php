49667939bb945e8151a97df7227b681499cfa79f
devchandan225 <chandant@taknify.com>
2025-12-16 12:07:44 +0545

    new

--- routes/web.php
+++ routes/web.php
@@ -103,12 +103,12 @@ Route::get('/publication/featured', [PublicPublicationController::class, 'featur
 Route::get('/publication/stats', [PublicPublicationController::class, 'stats'])->name('publications.stats');
 Route::get('/publication/{slug}', [PublicPublicationController::class, 'show'])->name('publication.show');
 
-// More Publications
-Route::get('/more-publication', [MorePublicationController::class, 'index'])->name('more-publications.index');
+// More Publications - Redirect to Publications
+Route::redirect('/more-publication', '/publication', 301)->name('more-publications.index');
+Route::redirect('/more-publication/{slug}', '/publication', 301)->name('more-publication.show');
 Route::get('/more-publications/search', [MorePublicationController::class, 'search'])->name('more-publications.search');
 Route::get('/more-publications/featured', [MorePublicationController::class, 'featured'])->name('more-publications.featured');
 Route::get('/more-publications/stats', [MorePublicationController::class, 'stats'])->name('more-publications.stats');
-Route::get('/more-publication/{slug}', [MorePublicationController::class, 'show'])->name('more-publication.show');
 
 // Service Locations
 Route::get('/service-locations', [ServiceLocationController::class, 'index'])->name('service-locations.index');

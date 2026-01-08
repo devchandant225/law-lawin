e332827cf95aa3961abe22ad3bd9e49fbbafb172
devchandan225 <chandant@taknify.com>
2026-01-08 18:03:40 +0545

    new

--- resources/views/practice/show.blade.php
+++ resources/views/practice/show.blade.php
@@ -85,7 +85,7 @@
 
                                         <!-- Content -->
                                         <div class="w-full lg:w-1/2">
-                                            <h2 class="text-2xl md:text-3xl font-bold mb-4">
+                                            <h2 class="text-2xl md:text-3xl font-bold mb-4 text-gray-600">
                                                 {{ $content->title }}
                                             </h2>
                                             @if ($content->description)
